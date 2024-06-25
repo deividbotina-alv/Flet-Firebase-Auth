5b870d1f702475ec7e9ae16121dc7a4071975506
deivid <deivid.johan.botina.monsalve@gmail.com>
2024-06-25 16:47:42 +0200

    [UPD] main classes created in pages

--- app.py
+++ app.py
@@ -8,14 +8,27 @@ class Main(UserControl):
     def __ini__(self, page: Page,):
         super().__init__()
         self.page = page
+        self.init_helper()
 
     def init_helper(self):
         self.page.on_route_change = self.on_route_change
+        self.page.go("/login")
 
     def on_route_change(self, route):
+        new_page = {
+            "/login": Login,
+            "/signup": Signup,
+            "/me": Dashboard,
+            "/forgotpassword": ForgotPassword,
+        }[self.page.route](self.page)
+
         self.page.views.clear()
         self.page.views.append(
-            View()
+            View(
+                route,
+                controls=[new_page]
+            )
         )
+        self.page.update()
 
 app(target=Main, assets_dir="assets", view=WEB_BROWSER)
