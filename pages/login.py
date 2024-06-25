f17d4bae11e4f0f41a219b1cda465f8bd76ce462
deivid <deivid.johan.botina.monsalve@gmail.com>
2024-06-25 17:28:46 +0200

    [UPD] Enter email address cell added

--- pages/login.py
+++ pages/login.py
@@ -1,8 +1,51 @@
 from flet import *
+from utils.colors import *
 
 class Login(Container):
     def __init__(self, page:Page):
         super().__init__()
+        self.alignment = alignment.center
         self.expand = True
-        self.bgcolor = "blue"
+        self.bgcolor = blue
+        self.email_box = Container(
+            content = TextField(
+                border = InputBorder.NONE,
+                content_padding = padding.only(top=0,bottom=0,right=20,left=20),
+                hint_style = TextStyle(
+                    size=12, color = "#858796"
+                ),
+                hint_text = "Enter email address ...",
+                cursor_color = "858796",
+                text_style = TextStyle(
+                    size = 14,
+                    color = "black"
+                ),
+            ),
+            border = border.all(width=1, color="bdcbf4"),
+            border_radius = 30
+        )
+        
+        self.content = Column(
+            alignment = "center",
+            horizontal_alignment="center",
+            controls = [
+                Container(
+                    width = 500,
+                    padding = 40,
+                    bgcolor = "white",
+                    content=Column(
+                        horizontal_alignment = "center",
+                        controls = [
+                            Text(
+                                value = "Welcome back!",
+                                size = 16,
+                                color = "black",
+                                text_align = "center"
+                            ),
+                            self.email_box,
+                        ]
+                    )
+                )
+            ]
+        )
         
